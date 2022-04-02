5be4173e866035520205b0ed6672d9a8360a1d36
ASGodolfim <stonegodolfim@gmail.com>
2022-04-02 17:58:45 -0300

    Atualização note

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('home')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+
+def contato(request):
+    return HttpResponse('contato')
