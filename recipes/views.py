95b9ba5ce60721a7b6538eec19f3bbbc3ac321db
ASGodolfim <stonegodolfim@gmail.com>
2022-04-02 19:21:06 -0300

    adição font awesome e icone

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Arthur',} status=404)
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-
-def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/home.html')
