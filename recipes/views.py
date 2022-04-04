3e238d39c154f6bb2b704bc07c8b5e6cbea29214
ASGodolfim <stonegodolfim@gmail.com>
2022-04-04 14:50:20 -0300

    push from note

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('home')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-
-def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/home.html')
