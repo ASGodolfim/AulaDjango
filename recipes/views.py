056ec5868c79df73f64087eea1186f7288fc2baa
ASGodolfim <stonegodolfim@gmail.com>
2022-06-09 14:16:32 -0300

    fix html description recipe title

--- recipes/views.py
+++ recipes/views.py
@@ -24,9 +24,6 @@ def category(request,  category_id):
 
 
 def recipes(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id,
-        is_published=True,
-    ).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
 
     return render(request, 'recipes/pages/recipe-view.html', context={'recipe': recipe, 'is_detail_page': True})
